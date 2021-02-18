f18998168c18b356c8ee8613f0f25fd2d25008b7
Shameel Fazul <shameel.fazul@mail.com>
2021-02-18 17:41:09 +0530

    Reaction App
    SRC

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <h1>Shameel Reaction App</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="end" />
+  <Results v-if="showScore" :score="score" />
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results'
+
+export default {
+  name: 'App',
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showScore: false
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.floor(Math.random() * 5000)
+      this.isPlaying = true
+      this.showScore = false
+    },
+    end(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showScore = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
